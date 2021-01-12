dcb1dabcd8a974898af570a36f6ddf0de8d7c922
tsun-code <61623464+tsun-code@users.noreply.github.com>
2021-01-12 09:38:23 +0800

    Testing an Angular Factory

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
-(function() {
+(function () {
     'use strict';
-  
-    angular.module('meetIrl', [
-      'ui.router'
-    ])
-    .config(function($urlRouterProvider) {
-      $urlRouterProvider.otherwise("/");
-    });
-  })();
+
+    // Creating the module and factory we referenced in the beforeEach blocks in our test file
+    angular.module('api.users', [])
+        .factory('Users', function () {
+            var Users = {};
+
+            // Users.method = function() {};
+
+            return Users;
+        });
+
+})();
--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -1,7 +1,17 @@
 // Be descriptive with titles here. The describe and it titles combined read like a sentence.
-describe('Users factory', function() {
-    it('has a dummy spec to test 2 + 2', function() {
-        // An intentionally failing test. No code within expect() will never equal 4.
-      expect(2+2).toEqual(4);
+describe('Users factory', function () {
+    var Users;
+
+    // Before each test load our api.users module
+    beforeEach(angular.mock.module('api.users'));
+
+    // Before each test set our injected Users factory (_Users_) to our local Users variable
+    beforeEach(inject(function (_Users_) {
+        Users = _Users_;
+    }));
+
+    // A simple test to verify the Users factory exists
+    it('should exist', function () {
+        expect(Users).toBeDefined();
     });
-  });
+});
