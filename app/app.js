63f87643ea31eacc90fe2dd53f8ed44a6eb063b3
tsun-code <61623464+tsun-code@users.noreply.github.com>
2021-01-12 10:51:04 +0800

    Adding Users to the Users Service

--- app/app.js
+++ app/app.js
@@ -1,14 +1,10 @@
-(function () {
+(function() {
     'use strict';
-
-    // Creating the module and factory we referenced in the beforeEach blocks in our test file
-    angular.module('api.users', [])
-        .factory('Users', function () {
-            var Users = {};
-
-            // Users.method = function() {};
-
-            return Users;
-        });
-
-})();
+  
+    angular.module('meetIrl', [
+      'ui.router'
+    ])
+    .config(function($urlRouterProvider) {
+      $urlRouterProvider.otherwise("/");
+    });
+  })();
--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -1,6 +1,37 @@
 // Be descriptive with titles here. The describe and it titles combined read like a sentence.
 describe('Users factory', function () {
     var Users;
+    // The array of users our factory will provide us
+    var userList = [
+        {
+            id: '1',
+            name: 'Jane',
+            role: 'Designer',
+            location: 'New York',
+            twitter: 'gijane'
+        },
+        {
+            id: '2',
+            name: 'Bob',
+            role: 'Developer',
+            location: 'New York',
+            twitter: 'billybob'
+        },
+        {
+            id: '3',
+            name: 'Jim',
+            role: 'Developer',
+            location: 'Chicago',
+            twitter: 'jimbo'
+        },
+        {
+            id: '4',
+            name: 'Bill',
+            role: 'Designer',
+            location: 'LA',
+            twitter: 'dabill'
+        }
+    ];
 
     // Before each test load our api.users module
     beforeEach(angular.mock.module('api.users'));
@@ -14,4 +45,20 @@ describe('Users factory', function () {
     it('should exist', function () {
         expect(Users).toBeDefined();
     });
+
+    // A set of tests for our Users.all() method
+    describe('.all()', function () {
+        // A simple test to verify the method all exists
+        it('should exist', function () {
+            expect(Users.all).toBeDefined();
+        });
+
+        // A test to verify that calling all() returns the array of users we hard-coded above
+        it('should return a hard-coded list of users', function () {
+            expect(Users.all()).toEqual(userList);
+        });
+    });
+
+
+
 });
